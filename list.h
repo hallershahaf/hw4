1d4aa83461e0b90779932b0c71ab885f444c6bce
Shay Agroskin <agrosshay@gmail.com>
2018-06-22 23:16:29 +0300

    Added some changes to list.h to make it compile

--- list.h
+++ list.h
@@ -36,14 +36,14 @@ struct list_head {
  * This is only for internal list manipulation where we know
  * the prev/next entries already!
  */
-static inline void __list_add(struct list_head *new,
+static inline void __list_add(struct list_head *new_n,
 			      struct list_head *prev,
 			      struct list_head *next)
 {
-	next->prev = new;
-	new->next = next;
-	new->prev = prev;
-	prev->next = new;
+	next->prev = new_n;
+	new_n->next = next;
+	new_n->prev = prev;
+	prev->next = new_n;
 }
 
 /**
@@ -54,9 +54,9 @@ static inline void __list_add(struct list_head *new,
  * Insert a new entry after the specified head.
  * This is good for implementing stacks.
  */
-static inline void list_add(struct list_head *new, struct list_head *head)
+static inline void list_add(struct list_head *new_n, struct list_head *head)
 {
-	__list_add(new, head, head->next);
+	__list_add(new_n, head, head->next);
 }
 
 /**
@@ -67,9 +67,9 @@ static inline void list_add(struct list_head *new, struct list_head *head)
  * Insert a new entry before the specified head.
  * This is useful for implementing queues.
  */
-static inline void list_add_tail(struct list_head *new, struct list_head *head)
+static inline void list_add_tail(struct list_head *new_n, struct list_head *head)
 {
-	__list_add(new, head->prev, head);
+	__list_add(new_n, head->prev, head);
 }
 
 /*
@@ -93,8 +93,8 @@ static inline void __list_del(struct list_head *prev, struct list_head *next)
 static inline void list_del(struct list_head *entry)
 {
 	__list_del(entry->prev, entry->next);
-	entry->next = (void *) 0;
-	entry->prev = (void *) 0;
+	entry->next = (struct list_head *) 0;
+	entry->prev = (struct list_head *) 0;
 }
 
 /**
